d6d48bf525943f14c56650c137fa241fb3f95e74
filip.fiat <filip.fiat@haufe-lexware.com>
2026-03-10 19:59:29 +0200

    Adding initial version

---
+++ src/main/java/ro/digitalstack/betfair/App.java
@@ -0,0 +1,13 @@
+package ro.digitalstack.betfair;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Betfair!" + args[0]);
+    }
+}
